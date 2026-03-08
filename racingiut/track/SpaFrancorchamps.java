3ac16e61f9d4602c668186201d5591b6c7fbf00c
Taseen Tajwar <taseentajwar@iut-dhaka.edu>
2026-03-08 10:50:04 +0600

    Add SpaFrancorchamps Track implementation
    Introduce SpaFrancorchamps.java in racingiut/track: a Track implementation that overrides trackInfo() to print "Track: Circuit de Spa-Francorchamps (Belgium)". Adds basic representation of the Spa-Francorchamps circuit for use by the track subsystem.

---
+++ racingiut/track/SpaFrancorchamps.java
@@ -0,0 +1,9 @@
+package track;
+
+public class SpaFrancorchamps implements Track {
+
+    @Override
+    public void trackInfo(){
+        System.out.println("Track: Circuit de Spa-Francorchamps (Belgium)");
+    }
+}
